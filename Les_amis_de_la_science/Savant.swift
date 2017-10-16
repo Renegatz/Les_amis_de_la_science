a21e5e632c864f5f359f2863d68be9b15dcfd26a
git <git@LD140-12-06285.local>
2017-10-16 11:29:51 -0400

    Avant présentation d'une cellule personnalisé

---
+++ Les_amis_de_la_science/Savant.swift
@@ -0,0 +1,24 @@
+//
+//  Savant.swift
+//  Les_amis_de_la_science
+//
+//  Created by git on 17-10-16.
+//  Copyright © 2017 git. All rights reserved.
+//
+
+import UIKit
+
+class Savant: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
